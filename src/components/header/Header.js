55d097654160742bc42814cf0c09cb50d38c7cf4
Nide17 <nidedrogba@gmail.com>
2020-11-13 05:58:45 +0200

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Header = () => {
-    return (
-        <div>
-            <h1>
-                Hello Test
-            </h1>
-        </div>
-    )
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            menu: false
+        };
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    toggleMenu() {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    render() {
+
+        const show = (this.state.menu) ? "show" : "";
+
+        return (
+
+
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+
+                {
+                    /* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button> */
+                }
+
+                <button className="navbar-toggler"
+                    type="button"
+                    onClick={
+                        this.toggleMenu
+                    } >
+                    <span className="navbar-toggler-icon" > </span> </button >
+
+                <a className="navbar-brand"
+                    href="#/" > Navbar </a>
+
+                {
+                    /* <div className="collapse navbar-collapse" id="navbarTogglerDemo03"> */
+                } <div className={
+                    "collapse navbar-collapse " + show
+                }>
+
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0" >
+                        <li className="nav-item active" >
+                            <a className="nav-link"
+                                href="#/" > Home < span className="sr-only" > (current) </span></a >
+                        </li> <li className="nav-item" >
+                            <a className="nav-link"
+                                href="#/" > Link </a> </li > </ul>
+
+                    <form className="form-inline my-2 my-lg-0" >
+                        <input className="form-control mr-sm-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search" />
+                        <button className="btn btn-outline-success my-2 my-sm-0"
+                            type="submit" > Search </button> </form>
+
+                </div> </nav >
+        )
+    }
 }
 
 export default Header
