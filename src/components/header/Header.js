b97e689b02afec2b1284cfd4387f336f64da4686
Nide17 <nidedrogba@gmail.com>
2020-11-13 06:50:22 +0200

    test for bs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -24,48 +24,54 @@ class Header extends Component {
 
         return (
 
+            <div className="container-fluid">
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+                <nav className="row no-gutters navbar navbar-expand-lg navbar-light bg-light" >
 
-                {
-                    /* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button> */
-                }
+                    {
+                        /* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span className="navbar-toggler-icon"></span>
+                                    </button> */
+                    }
 
-                <button className="navbar-toggler"
-                    type="button"
-                    onClick={
-                        this.toggleMenu
-                    } >
-                    <span className="navbar-toggler-icon" > </span> </button >
+                    <div className="col-2">
 
-                <a className="navbar-brand"
-                    href="#/" > Navbar </a>
+                        <button className="navbar-toggler" type="button" onClick={this.toggleMenu}>
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
-                {
-                    /* <div className="collapse navbar-collapse" id="navbarTogglerDemo03"> */
-                } <div className={
-                    "collapse navbar-collapse " + show
-                }>
+                        <a className="navbar-brand" href="#/"> Navbar </a>
+                    </div>
 
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0" >
-                        <li className="nav-item active" >
-                            <a className="nav-link"
-                                href="#/" > Home < span className="sr-only" > (current) </span></a >
-                        </li> <li className="nav-item" >
-                            <a className="nav-link"
-                                href="#/" > Link </a> </li > </ul>
+                    {
+                        /* <div className="collapse navbar-collapse" id="navbarTogglerDemo03"> */
+                    }
 
-                    <form className="form-inline my-2 my-lg-0" >
-                        <input className="form-control mr-sm-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0"
-                            type="submit" > Search </button> </form>
+                    <div className={"collapse navbar-collapse " + show}>
 
-                </div> </nav >
+                        <form className="form-inline my-2 my-lg-0">
+
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit"> Search</button>
+                        </form>
+
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#/"> Home<span className="sr-only">(current)</span>
+                                </a>
+                            </li>
+
+                            <li className="nav-item" >
+                                <a className="nav-link" href="#/"> Link </a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
