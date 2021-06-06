105ed897b3d98b526766e226a9777b3742f0d178
Nide17 <nidedrogba@gmail.com>
2021-06-06 09:45:34 +0200

    changed header to functional componenent

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,126 +1,120 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import logo from '../logo/Logo.svg'
 import Logosm from '../logo/Logosm.svg'
 
-class Header extends Component {
+const Header = () => {
 
-    constructor() {
-        super();
+    const [state, setState] = useState({
+        menu: false,
+        isBuying: false
+    })
 
-        this.state = {
-            menu: false,
-            isBuying: false 
-        };
-
-        this.toggleMenu = this.toggleMenu.bind(this);
-        this.ChangeBuying = this.ChangeBuying.bind(this);
-    }
-
-    toggleMenu() {
-        this.setState({
-            menu: !this.state.menu
+    const toggleMenu = () => {
+        setState({
+            menu: !state.menu
         })
     }
 
-    ChangeBuying() {
-        this.setState({
-            isBuying: !this.state.isBuying
+    const ChangeBuying = () => {
+        setState({
+            isBuying: !state.isBuying
         })
     }
 
-    render() {
+    const show = (state.menu) ? "show" : "";
 
-        const show = (this.state.menu) ? "show" : "";
+    return (
 
-        return (
+        <div className="container-fluid">
 
-            <div className="container-fluid">
+            <nav className="row navbar navbar-expand-lg navbar-light mx-0">
 
-                <nav className="row navbar navbar-expand-lg navbar-light mx-0">
+                {
+                    /* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button> */
+                }
 
-                    {
-                        /* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span className="navbar-toggler-icon"></span>
-                                    </button> */
-                    }
+                <div className="col-6 col-lg-3 navbar-toggler-holder">
 
-                    <div className="col-6 col-lg-3 navbar-toggler-holder">
+                    <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+                        {/* <span className="navbar-toggler-icon"></span> */}
+                        <span>
+                            <i className="fa fa-bars"></i>
+                        </span>
+                    </button>
 
-                        <button className="navbar-toggler" type="button" onClick={this.toggleMenu}>
-                            {/* <span className="navbar-toggler-icon"></span> */}
-                            <span>
-                                <i className="fa fa-bars"></i>
-                            </span>
-                        </button>
+                    <a className="navbar-brand d-none d-lg-flex" href="#/">
+                        <img src={logo} alt="logo" />
+                    </a>
 
-                        <a className="navbar-brand d-none d-lg-flex" href="#/">
-                            <img src={logo} alt="logo" />
-                        </a>
+                    <a className="navbar-brand-small d-block d-lg-none" href="#/">
+                        <img src={Logosm} alt="Logosm" />
+                    </a>
+                </div>
 
-                        <a className="navbar-brand-small d-block d-lg-none" href="#/">
-                            <img src={Logosm} alt="Logosm" />
-                        </a>
-                    </div>
+                {
+                    /* <div className="collapse navbar-collapse" id="navbarTogglerDemo03"> */
+                }
 
-                    {
-                        /* <div className="collapse navbar-collapse" id="navbarTogglerDemo03"> */
-                    }
-
-                    <div className="nav-buttons col-6 col-lg-3 d-flex d-lg-none px-0">
-                        <button className="search-sm" type="submit">
+                <div className="nav-buttons col-6 col-lg-3 d-flex d-lg-none px-0">
+                    <button className="search-sm" type="submit">
                         <i className="fa fa-search"></i>
-                        </button>
+                    </button>
 
-                        <button className="btn sell mr-3" type="button" onClick={this.ChangeBuying}>{this.state.isBuying ? 'Buy now' : 'Sell now'}</button>
-                    </div>
+                    <button className="btn sell mr-3" type="button" onClick={ChangeBuying}>{state.isBuying ? 'Buy now' : 'Sell now'}</button>
+                </div>
 
-                    <div className={"nav-search col-12 col-lg-6 collapse navbar-collapse " + show}>
+                <div className={"nav-search col-12 col-lg-6 collapse navbar-collapse " + show}>
 
-                        <form>
-                            <div className="form-group row mx-0">
-                                <input className="form-control" type="search" placeholder="What do you want to buy?" aria-label="Search" />
-                                <button type="submit">
+                    <form>
+                        <div className="form-group row mx-0">
+                            <input className="form-control" type="search" placeholder="What do you want to buy?" aria-label="Search" />
+                            <button type="submit">
                                 <i className="fa fa-search"></i>
-                                </button>
-                            </div>
-                        </form>
-
-                        {/* FOR MOBILE */}
-                        <div className={"nav-buttons col-8 col-lg-3 d-flex d-lg-none justify-content-around mr-4"}>
-                            <button className="btn my-1" type="button">Sign up</button>
-                            <button className="btn my-1" type="button">Login</button>
-
-                            <div className="buy mt-3">
-                                <h5>Buy</h5>
-                                <ul>
-                                    <li className="item-buy mt-3">Electronics<i className="fa fa-angle-right"></i></li>
-                                    <li className="item-buy mt-3">Fashion<i className="fa fa-angle-right"></i></li>
-                                    <li className="item-buy mt-3">Furniture<i className="fa fa-angle-right"></i></li>
-                                    <li className="item-buy mt-3">Vehicles<i className="fa fa-angle-right"></i></li>
-                                    <li className="item-buy mt-3">Books<i className="fa fa-angle-right"></i></li>
-                                    <li className="item-buy mt-3">Properties<i className="fa fa-angle-right"></i></li>
-                                    <li className="item-buy mt-3">Others<i className="fa fa-angle-right"></i></li>
-                                </ul>
-                            </div>
-
-                            <div className="help mt-3">
-                                <i className="fa fa-question-circle mr-2"></i>
-                                <p>Help</p>
-                            </div>
-
+                            </button>
                         </div>
-                    </div>
+                    </form>
 
-                    {/* FOR LARGE */}
-                    <div className={"nav-buttons col-6 col-lg-3 d-none d-lg-flex justify-content-around pl-0 mr-4"}>
-                        <button className="btn sell px-lg-2" type="button" onClick={this.ChangeBuying}>{this.state.isBuying ? 'Buy now!' : 'Sell now!'}</button>
-                        <button className="btn px-lg-2" type="button">Sign up</button>
-                        <button className="btn px-lg-2" type="button">Login</button>
+                    {/* FOR MOBILE */}
+                    <div className={"nav-buttons col-8 col-lg-3 d-flex d-lg-none justify-content-around mr-4"}>
+                        <button className="btn my-1" type="button">Sign up</button>
+                        <button className="btn my-1" type="button">Login</button>
+
+                        <div className="buy mt-3">
+                            <h5>Buy</h5>
+                            <ul>
+                                <li className="item-buy mt-3">Electronics<i className="fa fa-angle-right"></i></li>
+                                <li className="item-buy mt-3">Fashion<i className="fa fa-angle-right"></i></li>
+                                <li className="item-buy mt-3">Furniture<i className="fa fa-angle-right"></i></li>
+                                <li className="item-buy mt-3">Vehicles<i className="fa fa-angle-right"></i></li>
+                                <li className="item-buy mt-3">Books<i className="fa fa-angle-right"></i></li>
+                                <li className="item-buy mt-3">Properties<i className="fa fa-angle-right"></i></li>
+                                <li className="item-buy mt-3">Others<i className="fa fa-angle-right"></i></li>
+                            </ul>
+                        </div>
+
+                        <div className="help mt-3">
+                            <i className="fa fa-question-circle mr-2"></i>
+                            <p>Help</p>
+                        </div>
+
                     </div>
-                </nav>
-            </div>
-        )
-    }
+                </div>
+
+                {/* FOR LARGE */}
+                <div className={"nav-buttons col-6 col-lg-3 d-none d-lg-flex justify-content-around pl-0 mr-4"}>
+
+                    <button className="btn sell px-lg-2" type="button" onClick={ChangeBuying}>
+                        {state.isBuying ? 'Buy now!' : 'Sell now!'}
+                    </button>
+
+                    <button className="btn px-lg-2" type="button">Sign up</button>
+                    <button className="btn px-lg-2" type="button">Login</button>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Header
