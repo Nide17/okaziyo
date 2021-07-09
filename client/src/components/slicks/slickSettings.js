a70e5dde081e0d6ecb35eb1c8b7182efa979130b
Nide17 <nidedrogba@gmail.com>
2021-07-09 17:30:52 +0200

    infinite slick

--- client/src/components/slicks/slickSettings.js
+++ client/src/components/slicks/slickSettings.js
@@ -1,58 +1,59 @@
 const settings = {
-    dots: false,
-    draggable:true,
-    infinite: false,
-    lazyLoad: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    initialSlide: 0,
-    // rows: 2,
-    responsive: [
-        {
-            breakpoint: 1025,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              infinite: false,
-              dots: false
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 1.8,
-              slidesToScroll: 1,
-              initialSlide: 0
-            }
-          },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1.2,
-                slidesToScroll: 1,
-                initialSlide: 0
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                autoplay: true,
-              lazyLoad: true,
-                slidesToShow: 1.1,
-                slidesToScroll: 1,
-            }
-        },
-        {
-          breakpoint: 410,
-          settings: {
-              autoplay: true,
-            lazyLoad: true,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-          }
+  dots: false,
+  draggable: true,
+  infinite: false,
+  lazyLoad: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  // rows: 2,
+  responsive: [
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
       }
-    ]
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1.8,
+        slidesToScroll: 1,
+        initialSlide: 0
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1.2,
+        slidesToScroll: 1,
+        initialSlide: 0
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        autoplay: true,
+        lazyLoad: true,
+        infinite: true,
+        slidesToShow: 1.1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 410,
+      settings: {
+        autoplay: true,
+        lazyLoad: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 };
 
 export default settings
